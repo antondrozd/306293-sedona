cca2ca16c8cb2b1529f029917886e1f32dda7346
Anton Drozd <anton.drozd.95@gmail.com>
2018-12-06 19:10:47 +0200

    remove mqpacker

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
-var mqpacker = require('css-mqpacker');
+// var mqpacker = require('css-mqpacker');
 var minify = require('gulp-csso');
 var rename = require('gulp-rename');
 var imagemin = require('gulp-imagemin');
@@ -16,17 +16,20 @@ var run = require('run-sequence');
 var del = require('del');
 
 gulp.task('style', function() {
-  gulp.src('sass/style.scss')
+  gulp
+    .src('sass/style.scss')
     .pipe(plumber())
     .pipe(sass())
-    .pipe(postcss([
-      autoprefixer({browsers: [
-        'last 2 versions'
-      ]}),
-      mqpacker({
-        sort: true
-      })
-    ]))
+    .pipe(
+      postcss([
+        autoprefixer({
+          browsers: ['last 2 versions']
+        })
+        // mqpacker({
+        //   sort: true
+        // })
+      ])
+    )
     .pipe(gulp.dest('build/css'))
     .pipe(minify())
     .pipe(rename('style.min.css'))
@@ -35,21 +38,27 @@ gulp.task('style', function() {
 });
 
 gulp.task('symbols', function() {
-  return gulp.src('build/img/icons/*.svg')
+  return gulp
+    .src('build/img/icons/*.svg')
     .pipe(svgmin())
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
+    .pipe(
+      svgstore({
+        inlineSvg: true
+      })
+    )
     .pipe(rename('symbols.svg'))
     .pipe(gulp.dest('build/img'));
 });
 
 gulp.task('images', function() {
-  return gulp.src('build/img/**/*.{png,jpg,gif}')
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true})
-    ]))
+  return gulp
+    .src('build/img/**/*.{png,jpg,gif}')
+    .pipe(
+      imagemin([
+        imagemin.optipng({ optimizationLevel: 3 }),
+        imagemin.jpegtran({ progressive: true })
+      ])
+    )
     .pipe(gulp.dest('build/img'));
 });
 
@@ -71,20 +80,15 @@ gulp.task('clean', function() {
 });
 
 gulp.task('copy', function() {
-  return gulp.src([
-    'fonts/**/*.{woff,woff2}',
-    'img/**',
-    'js/**',
-    '*.html'
-    ], {
+  return gulp
+    .src(['fonts/**/*.{woff,woff2}', 'img/**', 'js/**', '*.html'], {
       base: '.'
     })
     .pipe(gulp.dest('build'));
 });
 
 gulp.task('html:copy', function() {
-  return gulp.src('*.html')
-    .pipe(gulp.dest('build'))
+  return gulp.src('*.html').pipe(gulp.dest('build'));
 });
 
 gulp.task('html:update', ['html:copy'], function() {
@@ -93,12 +97,5 @@ gulp.task('html:update', ['html:copy'], function() {
 });
 
 gulp.task('build', function(fn) {
-  run(
-    'clean',
-    'copy',
-    'style',
-    'images',
-    'symbols',
-    fn
-  );
-})
+  run('clean', 'copy', 'style', 'images', 'symbols', fn);
+});
